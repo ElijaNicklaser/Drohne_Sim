23a74deaaadf90ad3fe6c77156d6376228e85af8
ElijaNicklaser <elija.nicklaser@student.htldornbirn.at>
2024-04-03 11:25:55 +0200

    Nachbar 2.

---
+++ src/ENNL/Nachbar.java
@@ -0,0 +1,63 @@
+package ENNL;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+
+public class Nachbar extends SpielObjekt{
+        private Input input;
+        private Rectangle shape;
+        private float acceleration = 0.1f;
+
+    @Override
+        public void draw(Graphics g) {
+            this.getImage().drawCentered(this.getX(), this.getY());
+        }
+
+        public Nachbar(int x, int y, Image image, Input input) {
+            super(x, y, image);
+            this.input = input;
+            shape = new Rectangle(x, y, image.getWidth(), image.getHeight());
+        }
+
+        @Override
+        public Shape getShape() {
+            return shape;
+        }
+
+        @Override
+        public void update(int delta) {
+            boolean pressed = false;
+
+            if (input.isKeyDown(Input.KEY_A)) {
+                this.setX(this.getX() - (int) this.acceleration);
+                if ((this.getX() < this.getWith() / 2)) this.setX(this.getWith() / 2);
+                pressed = true;
+            }
+            if (input.isKeyDown(Input.KEY_D)) {
+                this.setX(this.getX() + (int) this.acceleration);
+                if ((this.getX() > (1024 - this.getWith() / 2))) this.setX(1024 - this.getWith() / 2);
+                pressed = true;
+            }
+
+            if (pressed) {
+                acceleration += delta;
+                if (acceleration > 10) acceleration = 100;
+
+            } else {
+                acceleration = 0.01f;
+            }
+            shape.setCenterX(this.getX());
+            shape.setCenterY(this.getY());
+
+        }
+
+        public boolean intersects(Shape shape) {
+            if (shape != null) {
+                return this.getShape().intersects(shape);
+            }
+            return false;
+        }
+}
---
+++ src/ENNL/SpielObjekt.java
@@ -0,0 +1,67 @@
+package ENNL;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.geom.Shape;
+
+public abstract class SpielObjekt {
+    private int x;
+    private int y;
+    private int with;
+    private int height;
+    private Image image;
+
+    public SpielObjekt(int x, int y, Image image) {
+        this.x = x;
+        this.y = y;
+        this.with = with;
+        this.height = height;
+        this.image = image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWith() {
+        return with;
+    }
+
+    public void setWith(int with) {
+        this.with = with;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public abstract void draw(Graphics g);
+
+    public abstract Shape getShape();
+
+    public abstract void update(int delta);
+}
